d979a89a6f9247da53dea000cfc82dd27aa77295
Alex Brandt <alunduil@alunduil.com>
2018-01-05 21:41:26 -0600

    create release 0.1.0.0

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ mkDerivation {
   pname = "network-arbitrary";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base network-uri ];
+  libraryHaskellDepends = [ base network-uri QuickCheck ];
   testHaskellDepends = [ base hspec network-uri QuickCheck ];
   homepage = "https://github.com/alunduil/network-arbitrary";
   description = "Arbitrary Instances for Network Types";
