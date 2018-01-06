3515b7196e17a421a694d4fc8cb3d4414c6bedef
Alex Brandt <alunduil@alunduil.com>
2018-01-05 19:46:08 -0600

    add URI Arbitrary instance

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, hspec, QuickCheck, stdenv }:
+{ mkDerivation, base, hspec, network-uri, QuickCheck, stdenv }:
 mkDerivation {
   pname = "network-arbitrary";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base ];
-  testHaskellDepends = [ base hspec QuickCheck ];
+  libraryHaskellDepends = [ base network-uri ];
+  testHaskellDepends = [ base hspec network-uri QuickCheck ];
   homepage = "https://github.com/alunduil/network-arbitrary";
   description = "Arbitrary Instances for Network Types";
   license = stdenv.lib.licenses.mit;
