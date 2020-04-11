cd638c85834c929a357f00e8b72e56a8219412a9
Alex Brandt <alunduil@gmail.com>
2020-04-10 18:51:04 -0700

    Update project structure.
    * Reorganize cabal file (cosmetic).
    * Apply brittany formatting to all source files.
    * Update gitignore from gitignore.io.

--- test/Network/HTTP/Media/MediaType/ArbitrarySpec.hs
+++ test/Network/HTTP/Media/MediaType/ArbitrarySpec.hs
@@ -6,23 +6,32 @@ License     : MIT
 
 Tests for "Network.HTTP.Media.MediaType.Arbitrary".
 -}
-module Network.HTTP.Media.MediaType.ArbitrarySpec (main, spec) where
+module Network.HTTP.Media.MediaType.ArbitrarySpec
+  ( main
+  , spec
+  )
+where
 
-import Prelude hiding (null)
+import           Prelude                 hiding ( null )
 
-import Data.ByteString (null)
-import Data.CaseInsensitive (original)
-import Network.HTTP.Media.MediaType (mainType, subType)
-import Test.Hspec (describe, hspec, Spec)
-import Test.Hspec.QuickCheck (prop)
+import           Data.ByteString                ( null )
+import           Data.CaseInsensitive           ( original )
+import           Network.HTTP.Media.MediaType   ( mainType
+                                                , subType
+                                                )
+import           Test.Hspec                     ( describe
+                                                , hspec
+                                                , Spec
+                                                )
+import           Test.Hspec.QuickCheck          ( prop )
 
-import Network.HTTP.Media.MediaType.Arbitrary ()
+import           Network.HTTP.Media.MediaType.Arbitrary
+                                                ( )
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
-  describe "properties" $
-    do prop "not . null . mainType" $ not . null . original . mainType
-       prop "not . null . subType"  $ not . null . original . subType
+spec = describe "properties" $ do
+  prop "not . null . mainType" $ not . null . original . mainType
+  prop "not . null . subType" $ not . null . original . subType
